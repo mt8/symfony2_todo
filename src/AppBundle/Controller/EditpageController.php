455d1243114b757df37d5e3cfd6bed15894b7219
mt8 <info@mt8.biz>
2017-02-25 07:57:49 +0900

    編集用ページのコントローラーを作成 php app/console generate:controller

---
+++ src/AppBundle/Controller/EditpageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class EditpageController extends Controller
+{
+	/**
+	 * @Route("/{id}/edit")
+	 * @param int $id
+	 */
+	public function indexAction( $id )
+	{
+		
+	}
+}
