35c71dc1ce0ccd4fba28142f356f5f908152fbce
mt8 <info@mt8.biz>
2017-02-25 20:51:54 +0900

    編集ページで編集を可能にした

--- src/AppBundle/Controller/EditpageController.php
+++ src/AppBundle/Controller/EditpageController.php
@@ -5,14 +5,52 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use AppBundle\Entity\task;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+
 class EditpageController extends Controller
 {
 	/**
 	 * @Route("/{id}/edit")
-	 * @param int $id
+	 * @ParamConverter("task", class="AppBundle:task")
+	 * @Method("get")
 	 */
-	public function indexAction( $id )
+	public function indexAction( $task )
 	{
+		$form = $this->createTaskForm($task);
+		return $this->render('Editpage/index.html.twig',[
+			'form' => $form->createView(),
+			'task' => $task,
+		]);
+	}
+
+	/**
+	 * @Route("/{id}/edit")
+	 * @ParamConverter("task", class="AppBundle:task")
+	 * @Method("post")
+	 */	
+	public function indexPostAction( Request $request, task $task )
+	{
+		$form = $this->createTaskForm($task);
+		$form->handleRequest($request);
 		
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->flush();
+			return $this->redirect($this->generateUrl('app_toppage_index'));
+		}
+		
+	}
+	
+	private function createTaskForm( $task )
+	{
+		return $this->createFormBuilder($task)
+				->add('name','text')
+				->add('limitDate','date')
+				->add('status','choice',['choices' => [ '未','中','済' ]])
+				->add('submit', 'submit',['label'=>'保存'])
+				->getForm();
 	}
 }
